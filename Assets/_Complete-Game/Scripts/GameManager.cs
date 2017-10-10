795b3753d11085dc72e75db548447fc833af0d54
yayoitamura <gh3520900@gmail.com>
2017-10-11 01:35:36 +0900

    add comment

--- Assets/_Complete-Game/Scripts/GameManager.cs
+++ Assets/_Complete-Game/Scripts/GameManager.cs
@@ -10,69 +10,74 @@ namespace Completed
     //GameManager
 	public class GameManager : MonoBehaviour
 	{
-		public float levelStartDelay = 2f;						//Time to wait before starting level, in seconds.
-		public float turnDelay = 0.1f;							//Delay between each Player turn.
-		public int playerFoodPoints = 100;						//Starting value for Player food points.
-		public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
+		public float levelStartDelay = 2f;                      //レベル開始前の待機時間
+		public float turnDelay = 0.1f;                          //Delay between each Player turn.各プレイヤーのターンの間のディレイ。
+		public int playerFoodPoints = 100;                      //プレーヤのゲーム開始時のfood points
+		public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.他のスクリプトがアクセスできるようにするGameManagerの静的インスタンス。
+																//他のスクリプトがアクセスできるようにするGameManagerの静的インスタンス。
 		[HideInInspector] public bool playersTurn = true;		//Boolean to check if it's players turn, hidden in inspector but public.
+                                                                //ブール値で、プレイヤーが回っているか、インスペクターではなくパブリックになっているかを調べます。
 		
 		
-		private Text levelText;									//Text to display current level number.
-		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
-		private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
-		private int level = 1;									//Current level number, expressed in game as "Day 1".
-		private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
-		private bool enemiesMoving;								//Boolean to check if enemies are moving.
-		private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
-		
-		
-		
-		//Awake is always called before any Start functions
+		private Text levelText;                                 //Text to display current level number.現在のレベル番号を表示するテキスト。
+		private GameObject levelImage;                          //Image to block out level as levels are being set up, background for levelText.レベルが設定されているのでレベルをブロックする画像、levelTextの背景。
+		private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.レベルを設定するBoardManagerへの参照を保存する
+		private int level = 1;                                  //Current level number, expressed in game as "Day 1".現在のレベル番号。ゲームでは「1日目」と表現されます。
+		private List<Enemy> enemies;                            //List of all Enemy units, used to issue them move commands.移動コマンドを発行するために使用されるすべての敵ユニットのリスト。
+		private bool enemiesMoving;                             //Boolean to check if enemies are moving.敵が動いているかどうかを調べるブール値。
+		private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
+																//ボードをセットアップしているかどうかを確認するブール値。セットアップ中にPlayerが移動しないようにします。
+
+
+
+		//Awake is always called before any Start functions   Awakeは常にStart関数の前に呼び出されます
 		void Awake()
 		{
-            //Check if instance already exists
-            if (instance == null)
+			//Check if instance already exists インスタンスがすでに存在するかどうかを確認する
+			if (instance == null)
 
-                //if not, set instance to this
-                instance = this;
+				//if not, set instance to this そうでない場合は、instanceをthisに設定します。
+				instance = this;
 
-            //If instance already exists and it's not this:
-            else if (instance != this)
+			//If instance already exists and it's not this: インスタンスが既に存在し、それがこれでない場合：
+			else if (instance != this)
 
                 //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-                Destroy(gameObject);	
-			
-			//Sets this to not be destroyed when reloading scene
+                //その後これを破壊する。 これにより、シングルトンのパターンが強制されます。つまり、GameManagerのインスタンスは1つしか存在できません。
+                Destroy(gameObject);
+
+			//Sets this to not be destroyed when reloading scene シーンをリロードするときに破棄しないように設定します
 			DontDestroyOnLoad(gameObject);
-			
-			//Assign enemies to a new List of Enemy objects.
+
+			//Assign enemies to a new List of Enemy objects. 敵を新しい敵のリストオブジェクトに割り当てます。
 			enemies = new List<Enemy>();
-			
-			//Get a component reference to the attached BoardManager script
+
+			//Get a component reference to the attached BoardManager script 付属のBoardManagerスクリプトへのコンポーネント参照を取得する
 			boardScript = GetComponent<BoardManager>();
-			
-			//Call the InitGame function to initialize the first level 
+
+			//Call the InitGame function to initialize the first level   InitGame関数を呼び出して最初のレベルを初期化する
 			InitGame();
 		}
 
         //this is called only once, and the paramter tell it to be called only after the scene was loaded
         //(otherwise, our Scene Load callback would be called the very first load, and we don't want that)
+        //これは一度だけ呼び出され、シーンがロードされた後に呼び出されるようにパラメータに指示します（そうでなければ、シーンロードコールバックは最初のロードと呼ばれ、
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static public void CallbackInitialization()
         {
-            //register the callback to be called everytime the scene is loaded
-            SceneManager.sceneLoaded += OnSceneLoaded;
+			//register the callback to be called everytime the scene is loaded シーンがロードされるたびに呼び出されるコールバックを登録する
+			SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
-        //This is called each time a scene is loaded.
-        static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+		//This is called each time a scene is loaded. これはシーンがロードされるたびに呼び出されます。
+		static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
             instance.level++;
             instance.InitGame();
         }
 
-		
-		//Initializes the game for each level.
+
+		//Initializes the game for each level. 各レベルのゲームを初期化します。
 		void InitGame()
 		{
 			//While doingSetup is true the player can't move, prevent player from moving while title card is up.
@@ -100,9 +105,9 @@ namespace Completed
 			boardScript.SetupScene(level);
 			
 		}
-		
-		
-		//Hides black image used between levels
+
+
+		//Hides black image used between levels レベル間で使用される黒い画像を非表示にする
 		void HideLevelImage()
 		{
 			//Disable the levelImage gameObject.
@@ -111,8 +116,8 @@ namespace Completed
 			//Set doingSetup to false allowing player to move again.
 			doingSetup = false;
 		}
-		
-		//Update is called every frame.
+
+		//Update is called every frame. 更新はすべてのフレームと呼ばれます。
 		void Update()
 		{
 			//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
@@ -126,14 +131,15 @@ namespace Completed
 		}
 		
 		//Call this to add the passed in Enemy to the List of Enemy objects.
+        //これを呼び出して、渡された敵を敵のリストオブジェクトに追加します。
 		public void AddEnemyToList(Enemy script)
 		{
 			//Add Enemy to List enemies.
 			enemies.Add(script);
 		}
-		
-		
-		//GameOver is called when the player reaches 0 food points
+
+
+		//GameOver is called when the player reaches 0 food points   GameOverは、プレイヤーが0点の食べ物ポイントに達すると呼び出されます
 		public void GameOver()
 		{
 			//Set levelText to display number of levels passed and game over message
@@ -145,8 +151,8 @@ namespace Completed
 			//Disable this GameManager.
 			enabled = false;
 		}
-		
-		//Coroutine to move enemies in sequence.
+
+		//Coroutine to move enemies in sequence. コルーチンは順番に敵を動かす。
 		IEnumerator MoveEnemies()
 		{
 			//While enemiesMoving is true player is unable to move.
--- Assets/_Complete-Game/Scripts/Player.cs
+++ Assets/_Complete-Game/Scripts/Player.cs
@@ -24,6 +24,8 @@ namespace Completed
 		
 		private Animator animator;					//animator componentを取得
 		private int food;                           //foodの合計point
+
+		//プリプロセッサ ディレクティブ
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         private Vector2 touchOrigin = -Vector2.one;	//Used to store location of screen touch origin for mobile controls.
         //プライベートVector2 touchOrigin = -Vector2.one; //モバイルコントロールのスクリーンタッチ原点の場所を格納するために使用されます。
@@ -38,149 +40,171 @@ namespace Completed
 
 			//Get the current food point total stored in GameManager.instance between levels.レベル間でGameManager.instanceに保存されている現在の食品ポイント合計を取得します。
 			food = GameManager.instance.playerFoodPoints;
-			
-			//Set the foodText to reflect the current player food total.
+
+			//Set the foodText to reflect the current player food total.現在のプレーヤーの食べ物の合計を反映するように食物テキストを設定します。
 			foodText.text = "Food: " + food;
-			
-			//Call the Start function of the MovingObject base class.
+
+			//Call the Start function of the MovingObject base class.MovingObject基本クラスのStart関数を呼び出します。
 			base.Start ();
 		}
-		
-		
-		//This function is called when the behaviour becomes disabled or inactive.
+
+
+		//This function is called when the behaviour becomes disabled or inactive.この関数は、ビヘイビアが無効または無効になったときに呼び出されます。
 		private void OnDisable ()
 		{
 			//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+            //Playerオブジェクトが無効になっているときは、現在のローカルフードの合計をGameManagerに格納して、次のレベルに再ロードすることができます。
 			GameManager.instance.playerFoodPoints = food;
 		}
 		
 		
 		private void Update ()
 		{
-			//If it's not the player's turn, exit the function.
+			//If it's not the player's turn, exit the function.プレイヤーのターンでない場合は、その機能を終了します。
 			if(!GameManager.instance.playersTurn) return;
 			
-			int horizontal = 0;  	//Used to store the horizontal move direction.
-			int vertical = 0;		//Used to store the vertical move direction.
-			
+			int horizontal = 0;     //Used to store the horizontal move direction.水平移動方向を格納するために使用します。
+			int vertical = 0;       //Used to store the vertical move direction.垂直移動方向を格納するために使用します。
+
 			//Check if we are running either in the Unity editor or in a standalone build.
+            //Unityエディタまたはスタンドアロンビルドで実行しているかどうかを確認します。
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
-			
+
 			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+            //入力マネージャから入力を取得し、整数に丸め、水平軸に格納してx軸の移動方向を設定します
 			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 			
 			//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+            //入力マネージャから入力を取得し、整数に丸め、垂直軸に格納してy軸の移動方向を設定します
 			vertical = (int) (Input.GetAxisRaw ("Vertical"));
-			
-			//Check if moving horizontally, if so set vertical to zero.
+
+			//Check if moving horizontally, if so set vertical to zero.水平方向に移動するかどうかを確認し、垂直方向にゼロに設定する。
 			if(horizontal != 0)
 			{
 				vertical = 0;
 			}
 			//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+			//私たちがiOS、Android、Windows Phone 8、またはUnity iPhoneで動作しているか確認してください
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 			
-			//Check if Input has registered more than zero touches
+			//Check if Input has registered more than zero touches入力がゼロ以上のタッチを登録しているかどうかを確認する
 			if (Input.touchCount > 0)
 			{
-				//Store the first touch detected.
+				//Store the first touch detected.最初に検出されたタッチを保存します。
 				Touch myTouch = Input.touches[0];
 				
-				//Check if the phase of that touch equals Began
+				//Check if the phase of that touch equals Beganそのタッチの位相がBeganに等しいかどうかを確認する
 				if (myTouch.phase == TouchPhase.Began)
 				{
-					//If so, set touchOrigin to the position of that touch
+					//If so, set touchOrigin to the position of that touch そうであれば、touchOriginをそのタッチの位置に設定します
 					touchOrigin = myTouch.position;
 				}
 				
 				//If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                //タッチ位相がBeganでなく、代わりにEndedに等しく、touchOriginのxがゼロ以上である場合：
 				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
 				{
-					//Set touchEnd to equal the position of this touch
+					//Set touchEnd to equal the position of this touchこのタッチの位置と等しくなるようにtouchEndを設定します
 					Vector2 touchEnd = myTouch.position;
 					
-					//Calculate the difference between the beginning and end of the touch on the x axis.
+					//Calculate the difference between the beginning and end of the touch on the x axis.x軸のタッチの開始と終了の差を計算します。
 					float x = touchEnd.x - touchOrigin.x;
 					
-					//Calculate the difference between the beginning and end of the touch on the y axis.
+					//Calculate the difference between the beginning and end of the touch on the y axis.タッチの開始と終了の差をy軸上で計算します。
 					float y = touchEnd.y - touchOrigin.y;
 					
 					//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                    //touchIrigin.xを-1に設定すると、else if文がfalseと評価され、直ちに繰り返されることはありません。
 					touchOrigin.x = -1;
 					
 					//Check if the difference along the x axis is greater than the difference along the y axis.
+                   //x軸に沿った差がy軸に沿った差よりも大きいかどうか確認してください。
 					if (Mathf.Abs(x) > Mathf.Abs(y))
-						//If x is greater than zero, set horizontal to 1, otherwise set it to -1
+						//If x is greater than zero, set horizontal to 1, otherwise set it to -1 xが0より大きい場合、horizontalを1に設定し、そうでない場合は-1に設定します
 						horizontal = x > 0 ? 1 : -1;
 					else
-						//If y is greater than zero, set horizontal to 1, otherwise set it to -1
+						//If y is greater than zero, set horizontal to 1, otherwise set it to -1 yが0より大きい場合は、horizontalを1に設定し、そうでない場合は-1に設定します
 						vertical = y > 0 ? 1 : -1;
 				}
 			}
 			
-#endif //End of mobile platform dependendent compilation section started above with #elif
+#endif //End of mobile platform dependendent compilation section started above with #elif #elifで上で起動したモバイルプラットフォーム依存のコンパイルセクションの終わり
 			//Check if we have a non-zero value for horizontal or vertical
 			if(horizontal != 0 || vertical != 0)
 			{
 				//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
 				//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+                //AttemptMoveはジェネリックパラメータWallを渡します。これはPlayerが攻撃を受けたときに対話することができるためです（
+                //水平方向と垂直方向のパラメータを渡してPlayerを移動する方向を指定します）
 				AttemptMove<Wall> (horizontal, vertical);
 			}
 		}
 		
 		//AttemptMove overrides the AttemptMove function in the base class MovingObject
 		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+        //AttemptMoveは、基底クラスのAttemptMove関数をオーバーライドします。MovingObject AttemptMoveは、PlayerがWall型のジェネリックパラメータTをとります.x方向とy方向の整数も移動します。
 		protected override void AttemptMove <T> (int xDir, int yDir)
 		{
-			//Every time player moves, subtract from food points total.
+			//Every time player moves, subtract from food points total.プレイヤーが移動するたびに、合計食糧ポイントを引く。
 			food--;
-			
-			//Update food text display to reflect current score.
+
+			//Update food text display to reflect current score.現在のスコアを反映するように食べ物のテキスト表示を更新します。
 			foodText.text = "Food: " + food;
 			
 			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+            //基本クラスのAttemptMoveメソッドを呼び出し、コンポーネントT（この場合はWall）とx方向とy方向を移動して渡します。
 			base.AttemptMove <T> (xDir, yDir);
 			
 			//Hit allows us to reference the result of the Linecast done in Move.
+            //ヒットは、我々が移動で行なわれたラインキャストの結果を参照することを可能にする。
 			RaycastHit2D hit;
 			
 			//If Move returns true, meaning Player was able to move into an empty space.
+            //Moveがtrueを返す場合、Playerは空の領域に移動できたことを意味します。
 			if (Move (xDir, yDir, out hit)) 
 			{
 				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+                //SoundManagerのRandomizeSfxを呼び出して、2つのオーディオクリップを渡して移動音を再生します。
 				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
 			}
 			
 			//Since the player has moved and lost food points, check if the game has ended.
+            //プレイヤーは移動して食糧を失ったので、ゲームが終了したかどうかを確認します。
 			CheckIfGameOver ();
 			
 			//Set the playersTurn boolean of GameManager to false now that players turn is over.
+            //プレイヤーが終了したら、GameManagerのplayersTurn booleanをfalseに設定します。
 			GameManager.instance.playersTurn = false;
 		}
 		
 		
 		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
 		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+        //OnCantMoveは、MovingObjectのOnCantMoveという抽象関数をオーバーライドします。 
+        //それはプレーヤーが攻撃で破壊することができる壁である場合、一般的なパラメータTをとります。
 		protected override void OnCantMove <T> (T component)
 		{
-			//Set hitWall to equal the component passed in as a parameter.
+			//Set hitWall to equal the component passed in as a parameter.パラメーターとして渡されたコンポーネントと等しくなるようにhitWallを設定します。
 			Wall hitWall = component as Wall;
-			
-			//Call the DamageWall function of the Wall we are hitting.
+
+			//Call the DamageWall function of the Wall we are hitting.私たちが襲っている壁のDamageWall機能を呼び出します。
 			hitWall.DamageWall (wallDamage);
 			
 			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
+            //プレイヤーの攻撃アニメーションを再生するために、プレイヤーのアニメーションコントローラーの攻撃トリガーを設定します。
 			animator.SetTrigger ("playerChop");
 		}
 		
 		
 		//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+        //OnTriggerEnter2Dは、別のオブジェクトがこのオブジェクトに接続されたトリガーコライダーに入ると送信されます（2D物理のみ）。
 		private void OnTriggerEnter2D (Collider2D other)
 		{
-			//Check if the tag of the trigger collided with is Exit.
+			//Check if the tag of the trigger collided with is Exit.衝突したトリガーのタグがExitかどうかを確認してください。
 			if(other.tag == "Exit")
 			{
 				//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+                //restartLevelDelayの遅延（デフォルトは1秒）で次のレベルを開始するには、Restart関数を呼び出します。
 				Invoke ("Restart", restartLevelDelay);
 				
 				//Disable the player object since level is over.
